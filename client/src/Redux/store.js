e311b8d74fa035dd4ab508c5dc0ea4208e75ded7
Julian Piñel <julianpiniel@gmail.com>
2021-07-29 15:09:33 -0300

    without redux tools

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -2,12 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
 import rootReducer from "./reducer";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(reduxThunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(reduxThunk)));
 
 export default store;
+//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
