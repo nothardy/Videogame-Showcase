bf090a47423b5c64ba00351b620b5c2e3ab6c8bf
Julian Piñel <julianpiniel@gmail.com>
2021-07-29 13:04:52 -0300

    deployment updates

--- api/index.js
+++ api/index.js
@@ -19,10 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { db } = require("./src/models");
-
 // Syncing all the models at once.
 db.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log("deployed"); // eslint-disable-line no-console
   });
 });
--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,12 +1,41 @@
 require("dotenv").config();
 var Sequelize = require("sequelize");
 
-const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env,
-  db = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-    { logging: false, native: false }
-  ),
-  { UUID, STRING, ARRAY, INTEGER, TEXT, BOOLEAN } = Sequelize.DataTypes;
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+// const db = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+//   { logging: false, native: false }
+// );
+const { UUID, STRING, ARRAY, INTEGER, TEXT, BOOLEAN } = Sequelize.DataTypes;
+
+let db =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASS,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+        { logging: false, native: false }
+      );
 
 const Game = db.define("game", {
   id: { type: UUID, allowNull: false, primaryKey: true },
